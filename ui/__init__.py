639625a4d9629bda4314f9aca0e4123e4b6fddab
unknown <1310819649@qq.com>
2021-10-08 18:11:39 +0800

    Initial commit

---
+++ ui/__init__.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : __init__.py.py
+# Time       ：2021/8/18 8:27
+# Author     ：王洪磊
+# Email      ： wang_hl007@163.com
+# Description：
+"""
